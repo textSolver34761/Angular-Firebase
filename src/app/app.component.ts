0fc40e2182bfd0b82547cc745e66bf3c466cfcce
textSolver34761 <pradonbenjamin.gmail.com>
2021-05-31 15:07:29 +0200

    firebase is configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase2';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCk0VtVp622CN-1KcdJndtUiDqJL__DQGU",
+      authDomain: "angular-40936.firebaseapp.com",
+      projectId: "angular-40936",
+      storageBucket: "angular-40936.appspot.com",
+      messagingSenderId: "692206017572",
+      appId: "1:692206017572:web:d04bef4349bdce98aa4cb1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
